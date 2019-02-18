da7a13c93738f3649a7f7f58b2fb24ac7653f8ac
Aditya Khatri <adityakhatri47@gmail.com>
2019-02-18 11:48:59 +0545

    Make navbar smaller and fixed

--- src/constants/iconNames.js
+++ src/constants/iconNames.js
@@ -3,6 +3,13 @@ const iconNames = {
     alert: 'ion-android-alert',
     menu: 'ion-md-menu',
     close: 'ion-close',
+
+    dashboard: 'ion-android-home',
+    riskMap: 'ion-android-map',
+    incidents: 'ion-alert-circled',
+    lossAndDamange: 'ion-ios-clock',
+    drrProfileMapping: 'ion-briefcase',
+    aboutUs: 'ion-ios-information',
 };
 
 export default iconNames;
