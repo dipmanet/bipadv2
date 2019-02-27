778e09c28f387f278464dfe7c21f6f778ddd2cb2
Aditya Khatri <adityakhatri47@gmail.com>
2019-02-27 16:25:44 +0545

    Support multipolygon alerts

--- src/views/Dashboard/Map/mapStyles.js
+++ src/views/Dashboard/Map/mapStyles.js
@@ -3,6 +3,11 @@ export const boundsFill = {
     'fill-opacity': 0.1,
 };
 
+export const polygonBoundsFill = {
+    'fill-color': 'red',
+    'fill-opacity': 0.4,
+};
+
 export const boundsOutline = {
     'line-color': '#4c4caa',
     'line-opacity': 0.2,
