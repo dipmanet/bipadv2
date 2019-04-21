5159ea69a2b121fc59cad0295d37d39ae2ad2cb5
tnagorra <weathermist@gmail.com>
2019-04-21 12:51:01 +0545

    Remove unused hazardIcon mapping

--- src/resources/data.js
+++ src/resources/data.js
@@ -1,56 +1,6 @@
-import landslideIcon from '#resources/icons/landslide.svg';
 import alertIcon from '#resources/icons/Alert.svg';
-import airCrashIcon from '#resources/icons/Airport-destroyed.svg';
-import animalTerrorIcon from '#resources/icons/Animal-terror.svg';
-import avalancheIcon from '#resources/icons/Snow-avalanche.svg';
-import boatCapsizeIcon from '#resources/icons/Boat.svg';
-import bridgeCollapseIcon from '#resources/icons/Bridge-destroyed.svg';
-import coldwaveIcon from '#resources/icons/Cold-wave.svg';
-import drowningIcon from '#resources/icons/Drowned.svg';
-import earthquakeIcon from '#resources/icons/Earthquake.svg';
-import epidemicIcon from '#resources/icons/Epidemic.svg';
-import fireIcon from '#resources/icons/Fire.svg';
-import floodIcon from '#resources/icons/Flood.svg';
-import forestFireIcon from '#resources/icons/Forest-fire.svg';
-import hailstoneIcon from '#resources/icons/Hailstone.svg';
-import heavyRainfallIcon from '#resources/icons/Heavy-rain.svg';
-import helicopterCrashIcon from '#resources/icons/Helicopter.svg';
-import highAltitudeIcon from '#resources/icons/High-altitude.svg';
-import otherIcon from '#resources/icons/Warning-Error.svg';
-import rainfallIcon from '#resources/icons/Rain.svg';
-import snakeBiteIcon from '#resources/icons/Snake.svg';
-import thunderboltIcon from '#resources/icons/Storm.svg';
-// import snowStormIcon from '#resources/icons/.svg';
-// import stormIcon from '#resources/icons/Storm.svg';
-// import windstormIcon from '#resources/icons/Storm-surge.svg';
-
 
 // eslint-disable-next-line import/prefer-default-export
 export const hazardIcons = {
-    1: airCrashIcon,
-    2: animalTerrorIcon,
-    3: avalancheIcon,
-    4: boatCapsizeIcon,
-    5: bridgeCollapseIcon,
-    6: coldwaveIcon,
-    7: drowningIcon,
-    8: earthquakeIcon,
-    9: epidemicIcon,
-    10: fireIcon,
-    11: floodIcon,
-    12: forestFireIcon,
-    13: hailstoneIcon,
-    14: heavyRainfallIcon,
-    15: helicopterCrashIcon,
-    16: highAltitudeIcon,
-    17: landslideIcon,
-    18: otherIcon,
-    19: rainfallIcon,
-    20: snakeBiteIcon,
-    // 21: snowStormIcon,
-    // 22: stormIcon,
-    23: thunderboltIcon,
-    // 24: windstormIcon,
-
     unknown: alertIcon,
 };
