e22efe67941e9f36c9c6aa98e79d2f7d437cd5f7
Aditya Khatri <adityakhatri47@gmail.com>
2019-04-25 07:26:27 +0545

    Separate stockpile search filter

---
+++ src/views/Response/Filter/StockPileFilter/index.js
@@ -0,0 +1,173 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import memoize from 'memoize-one';
+
+import { _cs, listToMap } from '@togglecorp/fujs';
+import Faram, {
+    FaramGroup,
+} from '@togglecorp/faram';
+
+import SelectInput from '#rsci/SelectInput';
+import NumberInput from '#rsci/NumberInput';
+import PrimaryButton from '#rsca/Button/PrimaryButton';
+
+import {
+    createConnectedRequestCoordinator,
+    createRequestClient,
+} from '#request';
+
+import {
+    setInventoryCategoryListActionRP,
+    setInventoryItemListActionRP,
+} from '#actionCreators';
+
+import {
+    inventoryCategoryListSelectorRP,
+    inventoryItemListSelectorRP,
+} from '#selectors';
+
+import { operatorOptions } from '../../resourceAttributes';
+import styles from './styles.scss';
+
+const requests = {
+    getInventoryCagetoriesRequest: {
+        url: '/inventory-category/',
+        onSuccess: ({ response, props: { setInventoryCategories } }) => {
+            setInventoryCategories({ inventoryCategoryList: response.results });
+        },
+        onMount: true,
+        // TODO: write schema
+    },
+
+    getInventoryItemsRequest: {
+        url: '/inventory-item/',
+        onSuccess: ({ response, props: { setInventoryItems } }) => {
+            setInventoryItems({ inventoryItemList: response.results });
+        },
+        onMount: true,
+        // TODO: write schema
+    },
+};
+
+const propTypes = {};
+const defaultProps = {};
+
+const labelSelector = x => x.label;
+const titleSelector = x => x.title;
+const keySelector = x => x.key;
+const idSelector = x => x.id;
+
+const mapStateToProps = state => ({
+    inventoryCategoryList: inventoryCategoryListSelectorRP(state),
+    inventoryItemList: inventoryItemListSelectorRP(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+    setInventoryCategories: params => dispatch(setInventoryCategoryListActionRP(params)),
+    setInventoryItems: params => dispatch(setInventoryItemListActionRP(params)),
+});
+
+class StockPileFilter extends React.PureComponent {
+    static propTypes = propTypes;
+    static defaultProps = defaultProps;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            faramValues: {},
+        };
+
+        this.schema = {
+            fields: {
+                quantity: [],
+                item: [],
+                operatorType: [],
+            },
+        };
+    }
+
+    getItemsUnits = memoize(inventoryItemList => listToMap(
+        inventoryItemList,
+        item => item.id,
+        item => item.unit,
+    ))
+
+    handleFaramChange = (faramValues) => {
+        this.setState({ faramValues });
+    }
+
+    handleFaramValidationSuccess = (_, faramValues) => {
+        this.props.setStockPileFilter(faramValues);
+    }
+
+    render() {
+        const {
+            faramValues,
+            faramErrors,
+        } = this.state;
+
+        const {
+            filteredList,
+            inventoryItemList,
+        } = this.props;
+
+        const itemUnits = this.getItemsUnits(inventoryItemList);
+
+        const { inventory: { item } = {} } = faramValues;
+        const unit = itemUnits[item];
+        const unitText = unit ? `(${unit})` : '';
+
+        return (
+            <Faram
+                className={styles.filterForm}
+                onChange={this.handleFaramChange}
+                onValidationSuccess={this.handleFaramValidationSuccess}
+                schema={this.schema}
+                value={faramValues}
+                error={faramErrors}
+            >
+                <h2 className={styles.heading}>
+                    Stockpile Items
+                </h2>
+                <SelectInput
+                    key="item"
+                    label="Item"
+                    faramElementName="item"
+                    keySelector={idSelector}
+                    labelSelector={titleSelector}
+                    options={inventoryItemList}
+                />
+                <NumberInput
+                    key="quantity"
+                    faramElementName="quantity"
+                    label={`Quantity${unitText}`}
+                    title="Quantity"
+                    separator=" "
+                />
+                <SelectInput
+                    key="operatorType"
+                    label="Operator"
+                    faramElementName="operatorType"
+                    keySelector={keySelector}
+                    labelSelector={labelSelector}
+                    options={operatorOptions}
+                />
+                <div className={styles.bottomContainer}>
+                    <PrimaryButton
+                        type="submit"
+                    >
+                        Search
+                    </PrimaryButton>
+                </div>
+            </Faram>
+        );
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+    createConnectedRequestCoordinator()(
+        createRequestClient(requests)(StockPileFilter),
+    ),
+);
