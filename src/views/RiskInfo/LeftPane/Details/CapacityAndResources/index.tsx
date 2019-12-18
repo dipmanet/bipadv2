c4e3127fae1b9f964629a5fe67ba84fb3da12b15
Sameer Rai <sameerai2736@gmail.com>
2019-12-18 17:45:41 +0545

    Add Capcity and Resource, Climate change data

--- src/views/RiskInfo/LeftPane/Details/CapacityAndResources/index.tsx
+++ src/views/RiskInfo/LeftPane/Details/CapacityAndResources/index.tsx
@@ -1,25 +1,69 @@
 import React from 'react';
-import { _cs } from '@togglecorp/fujs';
+import memoize from 'memoize-one';
+import {
+    _cs,
+    unique,
+
+} from '@togglecorp/fujs';
+
+import ListView from '#rscv/List/ListView';
+
+import { LayerWithGroup } from '#store/atom/page/types';
+
+import Group from '../Hazard/Group';
 
 import styles from './styles.scss';
 
+interface CapacityAndResourceGroup {
+    id: number;
+    title: string;
+    description: string;
+    layer: LayerWithGroup[];
+}
+
 interface Props {
     className?: string;
+    layerList: LayerWithGroup[];
 }
 
 interface State {
 }
 
+const keySelector = (d: CapacityAndResourceGroup) => d.id;
 
 export default class CapacityAndResources extends React.PureComponent<Props, State> {
+    private getGroupedLayers = memoize((layerList: LayerWithGroup[]) => {
+        const groups = unique(layerList.map(layer => layer.group), group => group.id) || [];
+
+        const groupWithLayers = groups.map((group) => {
+            const { id } = group;
+            const groupLayers = layerList.filter(layer => layer.group.id === id);
+
+            return ({ ...group, layers: groupLayers });
+        });
+
+        return groupWithLayers;
+    });
+
+    private getRendererParams = (_: number, group: CapacityAndResourceGroup) => group;
+
     public render() {
         const {
+            layerList,
             className,
         } = this.props;
 
+        const groups = this.getGroupedLayers(layerList);
+
         return (
             <div className={_cs(styles.capacityAndResources, className)}>
-                Capacity &amp; resources
+                <ListView
+                    className={styles.group}
+                    data={groups}
+                    keySelector={keySelector}
+                    renderer={Group}
+                    rendererParams={this.getRendererParams}
+                />
             </div>
         );
     }
--- src/views/RiskInfo/LeftPane/Details/ClimateChange/index.tsx
+++ src/views/RiskInfo/LeftPane/Details/ClimateChange/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import memoize from 'memoize-one';
+import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { _cs } from '@togglecorp/fujs';
 
@@ -12,6 +14,16 @@ import {
     Municipality,
     Ward,
 } from '#store/atom/page/types';
+import { MultiResponse } from '#store/atom/response/types';
+
+import {
+    createRequestClient,
+    NewProps,
+    ClientAttributes,
+    methods,
+} from '#request';
+
+import Loading from '#components/Loading';
 
 import chartImage from '#resources/images/mean-anual-precipitation-chart.png';
 import legendImage from '#resources/images/mean-anual-precipitation-legend.png';
@@ -27,11 +39,12 @@ import Map from './Map';
 
 import styles from './styles.scss';
 
+
 interface OwnProps {
     className?: string;
 }
 
-interface PropsFromAppState {
+interface PropsFromState {
     provinces: Province[];
     districts: District[];
     municipalities: Municipality[];
@@ -41,9 +54,48 @@ interface PropsFromAppState {
 interface PropsFromDispatch {
 }
 
-type Props = OwnProps & PropsFromAppState;
+interface Params {
+    setNapTemperature?: (data: NapTemperatureData[]) => void;
+    setNapPrecipitation?: (data: NapPrecipitationData[]) => void;
+}
+
+type ReduxProps = OwnProps & PropsFromDispatch & PropsFromState;
+type Props = NewProps<ReduxProps, Params>;
 
 interface State {
+    temperature: NapTemperatureData[];
+    precipitation: NapPrecipitationData[];
+    timePeriodKey: string;
+    measurementType: string;
+}
+
+interface NapTemperatureData {
+    id: number;
+    createdOn: string;
+    modifiedOn: string;
+    district: number;
+    temperatureRcp45: object;
+    sdRcp45: object;
+    temperatureRcp85: object;
+    sdRcp85: object;
+}
+
+interface NapPrecipitationData {
+    id: number;
+    createdOn: string;
+    modifiedOn: string;
+    district: number;
+    precipitationRcp45: object;
+    sdRcp45: object;
+    precipitationRcp85: object;
+    sdRcp85: object;
+}
+
+interface TimePeriod {
+    key: string;
+    label: string;
+    startYear: number;
+    endYear: number;
 }
 
 const mapStateToProps = (state: AppState) => ({
@@ -58,10 +110,10 @@ const measurementOptions = [
     { key: 'precipitation', label: 'Precipitation' },
 ];
 
-const timePeriodOptions = [
-    { key: 'reference-period', label: 'Reference period (1981-2010)' },
-    { key: 'medium-term', label: 'Medium term (2016-2065)' },
-    { key: 'long-term', label: 'Long Term (2036-2065)' },
+const timePeriodOptions: TimePeriod[] = [
+    { key: 'reference-period', label: 'Reference period (1981-2010)', startYear: 1981, endYear: 2010 },
+    { key: 'medium-term', label: 'Medium term (2016-2065)', startYear: 2016, endYear: 2065 },
+    { key: 'long-term', label: 'Long Term (2036-2065)', startYear: 2036, endYear: 2065 },
 ];
 
 const scenarioOptions = [
@@ -69,15 +121,161 @@ const scenarioOptions = [
     { key: 'rcp-85', label: 'RCP 8.5' },
 ];
 
+const requests: { [key: string]: ClientAttributes<ReduxProps, Params>} = {
+    napTemperatureGetRequest: {
+        url: '/nap-temperature/',
+        method: methods.GET,
+        onMount: true,
+        onSuccess: ({
+            response,
+            params: { setNapTemperature } = { setNapTemperature: undefined },
+        }) => {
+            const { results } = response as MultiResponse<NapTemperatureData>;
+            if (setNapTemperature) {
+                setNapTemperature(results);
+            }
+        },
+    },
+    napPrecipitationGetRequest: {
+        url: '/nap-precipitation/',
+        method: methods.GET,
+        onMount: true,
+        onSuccess: ({
+            response,
+            params: { setNapPrecipitation } = { setNapPrecipitation: undefined },
+        }) => {
+            const { results } = response as MultiResponse<NapPrecipitationData>;
+            if (setNapPrecipitation) {
+                setNapPrecipitation(results);
+            }
+        },
+    },
+};
+
 class ClimateChange extends React.PureComponent<Props, State> {
+    public constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            temperature: [],
+            precipitation: [],
+            timePeriodKey: 'reference-period',
+            measurementType: 'temeperature',
+        };
+
+        const {
+            requests: {
+                napTemperatureGetRequest,
+                napPrecipitationGetRequest,
+            },
+        } = this.props;
+
+        napTemperatureGetRequest.setDefaultParams({
+            setNapTemperature: (data: NapTemperatureData[]) => {
+                this.setState({
+                    temperature: data,
+                });
+            },
+        });
+
+        napPrecipitationGetRequest.setDefaultParams({
+            setNapPrecipitation: (data: NapPrecipitationData[]) => {
+                this.setState({
+                    precipitation: data,
+                });
+            },
+        });
+    }
+
+    private handleSetMeasurementType = (measurementType: string) => {
+        this.setState({
+            measurementType,
+        });
+    }
+
+    private handleSetTimePeriod = (timePeriodKey: string) => {
+        this.setState({
+            timePeriodKey,
+        });
+    }
+
+    private getFilteredTemperature = (temperature: NapTemperatureData[], timePeriodKey: string) => {
+        const timePeriod = timePeriodOptions.find(option => option.key === timePeriodKey)
+            || timePeriodOptions[0];
+        const { startYear, endYear } = timePeriod;
+
+        const data = temperature.map((temp) => {
+            const { temperatureRcp45, sdRcp45, temperatureRcp85, sdRcp85 } = temp;
+            const filter = (key: string) => (+key >= startYear && +key <= endYear);
+            const filteredTemperatureRcp45 = Object.keys(temperatureRcp45).filter(filter);
+            const filteredsdRcp45 = Object.keys(sdRcp45).filter(filter);
+            const filteredTempereatureRcp85 = Object.keys(temperatureRcp85).filter(filter);
+            const filteredsdRcp85 = Object.keys(sdRcp85).filter(filter);
+            return ({
+                ...temp,
+                temperatureRcp45: filteredTemperatureRcp45,
+                sdRcp45: filteredsdRcp45,
+                temperatureRcp85: filteredTempereatureRcp85,
+                sdRcp85: filteredsdRcp85,
+            });
+        });
+
+        return data;
+    }
+
+    private getFilteredPrecipitation = memoize(
+        (precipitation: NapPrecipitationData[], timePeriodKey: string) => {
+            const timePeriod = timePeriodOptions.find(option => option.key === timePeriodKey)
+                || timePeriodOptions[0];
+            const { startYear, endYear } = timePeriod;
+
+            const data = precipitation.map((rain) => {
+                const { precipitationRcp45, sdRcp45, precipitationRcp85, sdRcp85 } = rain;
+                const filter = (key: string) => (+key >= startYear && +key <= endYear);
+                const filteredPrecipitationeRcp45 = Object.keys(precipitationRcp45).filter(filter);
+                const filteredsdRcp45 = Object.keys(sdRcp45).filter(filter);
+                const filteredPrecipitationRcp85 = Object.keys(precipitationRcp85).filter(filter);
+                const filteredsdRcp85 = Object.keys(sdRcp85).filter(filter);
+                return ({
+                    ...rain,
+                    precipitationRcp45: filteredPrecipitationeRcp45,
+                    sdRcp45: filteredsdRcp45,
+                    precipitationRcp85: filteredPrecipitationRcp85,
+                    sdRcp85: filteredsdRcp85,
+                });
+            });
+            return data;
+        },
+    );
+
     public render() {
         const {
             districts,
             className,
+            requests: {
+                napPrecipitationGetRequest: {
+                    pending: precipitationPending,
+                },
+                napTemperatureGetRequest: {
+                    pending: temperaturePending,
+                },
+            },
         } = this.props;
+
+        const {
+            temperature,
+            precipitation,
+            timePeriodKey,
+            measurementType,
+        } = this.state;
+
+        const pending = precipitationPending || temperaturePending;
+        const data = measurementType === 'temperature' ? this.getFilteredTemperature(temperature, timePeriodKey)
+            : this.getFilteredPrecipitation(precipitation, timePeriodKey);
 
         return (
             <>
+                <Loading pending={pending} />
                 <Map
                     districts={districts}
                 />
@@ -87,13 +285,15 @@ class ClimateChange extends React.PureComponent<Props, State> {
                             className={styles.measurementTypeInput}
                             label="Measurement"
                             options={measurementOptions}
-                            value="temperature"
+                            value={measurementType}
+                            onChange={this.handleSetMeasurementType}
                         />
                         <SelectInput
                             label="Time period"
                             className={styles.timePeriodInput}
                             options={timePeriodOptions}
-                            value="medium-term"
+                            value={timePeriodKey}
+                            onChange={this.handleSetTimePeriod}
                         />
                         <SegmentInput
                             className={styles.scenarioInput}
@@ -120,6 +320,7 @@ class ClimateChange extends React.PureComponent<Props, State> {
     }
 }
 
-export default connect(mapStateToProps)(
-    ClimateChange,
-);
+export default compose(
+    connect(mapStateToProps),
+    createRequestClient(requests),
+)(ClimateChange);
