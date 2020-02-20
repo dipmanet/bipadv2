bcc1329028c551083c015749bfca19688e6e0a9a
frozenhelium <fren.ankit@gmail.com>
2020-02-20 14:34:03 +0545

    Improve throttling

--- src/components/SVGMapIcon/index.tsx
+++ src/components/SVGMapIcon/index.tsx
@@ -62,9 +62,9 @@ class SVGMapIcon extends React.PureComponent<Props, State> {
                         };
                         image.src = c.toDataURL();
                     });
-                }, Math.floor(33 + Math.random() * 12000));
+                }, Math.floor(100 + Math.random() * 16000));
             });
-        }, 0);
+        }, 100);
     })
 
     public render() {
