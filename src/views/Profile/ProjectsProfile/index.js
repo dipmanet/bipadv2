ed641d44e1c742e01ad347784ec92340ad5cb9b7
frozenhelium <fren.ankit@gmail.com>
2020-02-24 10:26:47 +0545

    Fix default IP for PIMS server

--- src/views/Profile/ProjectsProfile/index.js
+++ src/views/Profile/ProjectsProfile/index.js
@@ -251,7 +251,7 @@ const mapStateToProps = (state, props) => ({
     filters: projectsProfileFiltersSelector(state),
 });
 
-const wsEndpoint = process.env.REACT_APP_PROJECT_SERVER_URL || 'http://54.185.195.189';
+const wsEndpoint = process.env.REACT_APP_PROJECT_SERVER_URL || 'http://165.22.215.64/';
 
 const requests = {
     ndrrsapRequest: {
