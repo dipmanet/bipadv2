f644b9cda8a790f892cd7c9f3a6ff36f7832f115
Srijan Manandhar <srijanmanandhar12@gmail.com>
2020-10-20 16:12:44 +0545

    Added sorting logic to grouped items and minor stylings

--- src/views/DataArchive/LeftPane/Earthquake/EarthquakeGroup/index.tsx
+++ src/views/DataArchive/LeftPane/Earthquake/EarthquakeGroup/index.tsx
@@ -9,6 +9,17 @@ interface Props {
     address: ReactText;
     data: PageType.DataArchiveEarthquake[];
 }
+const SORT_KEY = 'eventOn';
+
+const compare = (a: any, b: any) => {
+    if (a[SORT_KEY] < b[SORT_KEY]) {
+        return 1;
+    }
+    if (a[SORT_KEY] > b[SORT_KEY]) {
+        return -1;
+    }
+    return 0;
+};
 
 const EarthquakeGroup = (props: Props) => {
     const [isExpanded, setIsExpanded] = useState(false);
@@ -23,9 +34,9 @@ const EarthquakeGroup = (props: Props) => {
                     {address}
                 </div>
                 <div className={styles.right}>
-                    <div className={styles.count}>
-                        {`${data.length} events`}
-                    </div>
+                    {/* <div className={styles.count}>
+                        {`${data.length} `}
+                    </div> */}
                     <Button
                         className={styles.chevron}
                         iconName={isExpanded ? 'chevronUp' : 'chevronDown'}
@@ -35,12 +46,19 @@ const EarthquakeGroup = (props: Props) => {
             </div>
 
             <div className={styles.child}>
-                { isExpanded && data.map((datum: PageType.DataArchiveEarthquake) => (
-                    <EarthquakeItem
-                        key={datum.id}
-                        data={datum}
-                    />
-                )) }
+                { isExpanded
+                && data
+                    .sort(compare)
+                    .map((datum: PageType.DataArchiveEarthquake) => (
+                        <div
+                            className={styles.wrapper}
+                            key={datum.id}
+                        >
+                            <EarthquakeItem
+                                data={datum}
+                            />
+                        </div>
+                    )) }
             </div>
 
         </div>
