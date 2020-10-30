98f9ceb922bc5357a1b70766eee8550afac60d91
Srijan Manandhar <srijanmanandhar12@gmail.com>
2020-10-30 12:58:26 +0545

    Added proper color to aqi chart

--- src/views/DataArchive/LeftPane/Pollution/Visualization/AqiChart/constants.ts
+++ src/views/DataArchive/LeftPane/Pollution/Visualization/AqiChart/constants.ts
@@ -1,9 +1,9 @@
-const GOOD_COLOR = '#009966';
-const MODERATE_COLOR = '#ffde33';
-const UNHEALTHY_FOR_SENSITIVE_COLOR = '#ff9933';
-const UNHEALTHY_COLOR = '#cc0033';
-const VERY_UNHEALTHY_COLOR = '#660099';
-const HAZARDOUS_COLOR = '#7e0023';
+const GOOD_COLOR = '#00fa2f';
+const MODERATE_COLOR = '#f7ff00';
+const UNHEALTHY_FOR_SENSITIVE_COLOR = '#ff7300';
+const UNHEALTHY_COLOR = '#ff0000';
+const VERY_UNHEALTHY_COLOR = '#9e0095';
+const HAZARDOUS_COLOR = '#8a0014';
 
 export const stackedBars = [
     { dataKey: 'good', stackId: 'pollution', fill: GOOD_COLOR },
