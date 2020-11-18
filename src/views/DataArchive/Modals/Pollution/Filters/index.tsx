1193ecd7f9511e27487b61d729a8263d409d3df6
Srijan Manandhar <srijanmanandhar12@gmail.com>
2020-11-18 10:14:17 +0545

    Added basic filters section

---
+++ src/views/DataArchive/Modals/Pollution/Filters/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './styles.scss';
+
+const Filters = () => {
+    console.log('FIlters');
+    return (
+        <div className={styles.filters}>
+            Filters
+        </div>
+    );
+};
+
+export default Filters;
