970bce3af243cf5b111097fb4d8502041f2b4cb7
Srijan Manandhar <srijanmanandhar12@gmail.com>
2020-11-18 11:06:13 +0545

    Added basic styling to Pollution popup filter

--- src/views/DataArchive/Modals/Pollution/Filters/index.tsx
+++ src/views/DataArchive/Modals/Pollution/Filters/index.tsx
@@ -2,13 +2,38 @@ import React from 'react';
 
 import styles from './styles.scss';
 
-const Filters = () => {
-    console.log('FIlters');
-    return (
-        <div className={styles.filters}>
-            Filters
+const Filters = () => (
+    <div className={styles.filters}>
+        <div className={styles.header}>
+                Filters
         </div>
-    );
-};
+        <div className={styles.selectors}>
+            <div className={styles.date}>
+                <div className={styles.title}>
+                    Date Selectors
+                </div>
+                <div className={styles.element}>
+                    Date Element
+                </div>
+            </div>
+            <div className={styles.parameters}>
+                <div className={styles.title}>
+                    Parameter Selector
+                </div>
+                <div className={styles.element}>
+                    Parameter Element
+                </div>
+            </div>
+            <div className={styles.period}>
+                <div className={styles.title}>
+                    Period Selector
+                </div>
+                <div className={styles.element}>
+                    Period Element
+                </div>
+            </div>
+        </div>
+    </div>
+);
 
 export default Filters;
