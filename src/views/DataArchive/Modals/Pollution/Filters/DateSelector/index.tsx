b7005f104a2d3d670c730d626b60d1a3756702e8
Srijan Manandhar <srijanmanandhar12@gmail.com>
2020-11-18 17:01:26 +0545

    Added date selector skeleton code in pollution popup

--- src/views/DataArchive/Modals/Pollution/Filters/DateSelector/index.tsx
+++ src/views/DataArchive/Modals/Pollution/Filters/DateSelector/index.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DateInput from '#rsci/DateInput';
 
 import styles from './styles.scss';
 
 const DateSelector = () => {
-    console.log('Date selector');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+
+    const handleStartDateChange = (newStartDate: string) => {
+        setStartDate(newStartDate);
+    };
+
+    const handleEndDateChange = (newEndDate: string) => {
+        setEndDate(newEndDate);
+    };
+
     return (
         <div className={styles.dateSelector}>
             <DateInput
                 label="Start Date"
                 className={styles.input}
+                value={startDate}
+                onChange={handleStartDateChange}
             />
             <div className={styles.seperator}>
                 To
@@ -17,6 +29,8 @@ const DateSelector = () => {
             <DateInput
                 label="End Date"
                 className={styles.input}
+                value={endDate}
+                onChange={handleEndDateChange}
             />
         </div>
     );
