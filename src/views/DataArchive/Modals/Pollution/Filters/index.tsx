6c0c1915424d7edea8a050740a01d89267890d17
Srijan Manandhar <srijanmanandhar12@gmail.com>
2020-11-19 12:22:10 +0545

    Added submit button to pollution pop up filter

--- src/views/DataArchive/Modals/Pollution/Filters/index.tsx
+++ src/views/DataArchive/Modals/Pollution/Filters/index.tsx
@@ -6,35 +6,47 @@ import PeriodSelector from './PeriodSelector';
 
 import styles from './styles.scss';
 
-const Filters = () => (
-    <div className={styles.filters}>
-        <div className={styles.header}>
+const Filters = () => {
+    const handleSubmitClick = () => {
+        console.log('Submit clicked');
+    };
+    return (
+        <div className={styles.filters}>
+            <div className={styles.header}>
                 Filters
-        </div>
-        <div className={styles.selectors}>
-            <div className={styles.date}>
-                <div className={styles.element}>
-                    <DateSelector />
-                </div>
             </div>
-            <div className={styles.parameters}>
-                <div className={styles.title}>
+            <div className={styles.selectors}>
+                <div className={styles.date}>
+                    <div className={styles.element}>
+                        <DateSelector />
+                    </div>
+                </div>
+                <div className={styles.parameters}>
+                    <div className={styles.title}>
                     Parameter Selector
+                    </div>
+                    <div className={styles.element}>
+                        <ParameterSelector />
+                    </div>
                 </div>
-                <div className={styles.element}>
-                    <ParameterSelector />
+                <div className={styles.period}>
+                    <div className={styles.title}>
+                    Period Selector
+                    </div>
+                    <div className={styles.element}>
+                        <PeriodSelector />
+                    </div>
                 </div>
             </div>
-            <div className={styles.period}>
-                <div className={styles.title}>
-                    Period Selector
-                </div>
-                <div className={styles.element}>
-                    <PeriodSelector />
-                </div>
+            <div
+                onClick={handleSubmitClick}
+                className={styles.submitButton}
+                role="presentation"
+            >
+                Submit
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Filters;
