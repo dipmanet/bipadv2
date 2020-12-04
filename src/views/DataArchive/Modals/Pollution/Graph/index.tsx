8f8467b61f1ce162f2e959e90d4c49fa9aedc5cc
Srijan Manandhar <srijanmanandhar12@gmail.com>
2020-12-04 13:59:45 +0545

    Added download feature and header to pollution pop up chart

--- src/views/DataArchive/Modals/Pollution/Graph/index.tsx
+++ src/views/DataArchive/Modals/Pollution/Graph/index.tsx
@@ -10,6 +10,10 @@ import {
     Tooltip,
     CartesianGrid,
 } from 'recharts';
+import Button from '#rsca/Button';
+import {
+    saveChart,
+} from '#utils/common';
 import { ArchivePollution, ChartData } from '../types';
 
 import NoData from '../NoData';
@@ -19,9 +23,19 @@ interface Props {
     stationData: ArchivePollution[];
     filterWiseChartData?: ChartData[];
     parameterCode?: string;
+    downloadId: string;
+    chartTitle?: string;
 }
+
+const DEFAULT_CHART_TITLE = 'Period Parameter Graph';
+const DEFAULT_DOWNLOAD_ID = 'pollutionPopUpChart';
+
+const handleSaveClick = (downloadId: string) => {
+    saveChart(downloadId || DEFAULT_DOWNLOAD_ID, downloadId || DEFAULT_DOWNLOAD_ID);
+};
+
 const Graph = (props: Props) => {
-    const { stationData, filterWiseChartData, parameterCode } = props;
+    const { stationData, filterWiseChartData, parameterCode, chartTitle, downloadId } = props;
     const code = parameterCode ? parameterCode.replace('.', '') : '';
     if (stationData.length === 0) {
         return (
@@ -29,23 +43,48 @@ const Graph = (props: Props) => {
         );
     }
     return (
-        <div
-            className={styles.chart}
-        // id={downloadId}
-        >
-            <ResponsiveContainer className={styles.container}>
-                <BarChart
-                    data={filterWiseChartData}
-                    margin={{ top: 10, right: 10, left: 10, bottom: 5 }}
+        <div className={styles.visualizations}>
+            <div
+                className={styles.periodParameterChart}
+            >
+                <header className={styles.header}>
+                    <h4 className={styles.heading}>
+                        {chartTitle || DEFAULT_CHART_TITLE}
+                    </h4>
+                    <div
+                        className={styles.downloadGroup}
+                        title="Download Chart"
+                        role="presentation"
+                        onClick={() => handleSaveClick(downloadId)}
+                    >
+                        <h4>Download</h4>
+                        <Button
+                            className={styles.chartDownload}
+                            transparent
+                            iconName="download"
+                        />
+                    </div>
+
+                </header>
+                <div
+                    className={styles.chart}
+                    id={downloadId || DEFAULT_DOWNLOAD_ID}
                 >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="label" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey={code} fill="#8884d8" />
-                </BarChart>
-            </ResponsiveContainer>
+                    <ResponsiveContainer className={styles.container}>
+                        <BarChart
+                            data={filterWiseChartData}
+                            margin={{ top: 10, right: 10, left: 10, bottom: 5 }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="label" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey={code} fill="#8884d8" />
+                        </BarChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
         </div>
     );
 };
