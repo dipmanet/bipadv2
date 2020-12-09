0c9361e5de0a590c2e52f2ba59c1c359cffb716c
Srijan Manandhar <srijanmanandhar12@gmail.com>
2020-12-09 09:58:58 +0545

    Added custom title feature to NoData view

--- src/views/DataArchive/Modals/Pollution/NoData/index.tsx
+++ src/views/DataArchive/Modals/Pollution/NoData/index.tsx
@@ -4,15 +4,23 @@ import styles from './styles.scss';
 
 interface Props {
     message?: string;
+    title?: string;
 }
 
 const DEFAULT_MESSAGE = 'No data available for selected Filter';
 
 const NoData = (props: Props) => {
-    const { message } = props;
+    const { message, title } = props;
     return (
         <div className={styles.noData}>
-            { message || DEFAULT_MESSAGE }
+            <div className={styles.title}>
+                {title}
+            </div>
+            <div className={styles.message}>
+                <div>
+                    { message || DEFAULT_MESSAGE }
+                </div>
+            </div>
         </div>
     );
 };
