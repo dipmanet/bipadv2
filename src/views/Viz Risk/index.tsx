7593562189ca246577363fe3c708e8b823a321e5
Ankur Acharya <ankur@youthinnovationlab.org>
2021-01-02 18:57:39 +0545

    added a boilerplate for the vr module

---
+++ src/views/Viz Risk/index.tsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class VizRisk extends PureComponent {
+    public render() {
+        return (
+            <h1>Viz Risk module</h1>
+        );
+    }
+}
+
+export default VizRisk;
