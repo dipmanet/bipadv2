85a189cc374cdefb1f1349505f3ac0924bec10fe
Srijan Manandhar <srijanmanandhar12@gmail.com>
2021-02-03 15:28:12 +0545

    Added Rain and River filter code

--- src/views/DataArchive/utils.ts
+++ src/views/DataArchive/utils.ts
@@ -110,3 +110,20 @@ export const getSpecificDate = (date: string, datePeriod: DatePeriod) => {
     };
     return dateObject[datePeriod];
 };
+
+export const isValidDate = (dateString: string = '') => {
+    const regEx = /^\d{4}-\d{2}-\d{2}$/;
+    if (!dateString.match(regEx)) return false; // Invalid format
+    const d = new Date(dateString);
+    const dNum = d.getTime();
+    if (!dNum && dNum !== 0) return false; // NaN value, Invalid date
+    return d.toISOString().slice(0, 10) === dateString;
+};
+
+export const getDateDiff = (startDate: string = '', endDate: string = '') => {
+    const date1 = new Date(startDate);
+    const date2 = new Date(endDate);
+    const diffTime = Math.abs(Number(date2) - Number(date1));
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return diffDays;
+};
