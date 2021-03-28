195125788bd487bfe5c268ba2d05c635f02e75ca
Ankur Acharya <ankur@youthinnovationlab.org>
2021-03-28 22:16:13 +0545

    added susecp legends

--- src/views/VizRisk/BarabiseLandslide/Components/Legends/legends.ts
+++ src/views/VizRisk/BarabiseLandslide/Components/Legends/legends.ts
@@ -11,4 +11,16 @@ export default {
             { key: 7, color: 'rgb(247,246,241)', label: 'Other' },
         ],
     },
+    7: {
+        title: 'LandSlide Suseptibility',
+        legends: [
+            { key: 1, color: 'rgb(8,120,188)', label: '0' },
+            { key: 2, color: 'rgb(47,140,197)', label: '0.05' },
+            { key: 3, color: '#fff', label: 'Bridge' },
+            { key: 4, color: 'rgb(90,176,0)', label: 'Road' },
+            { key: 5, color: '#0670bc', label: 'Water Bodies' },
+            { key: 6, color: '#aaa', label: 'Buildings' },
+            { key: 7, color: 'rgb(247,246,241)', label: 'Other' },
+        ],
+    },
 };
