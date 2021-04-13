8916967268eebc46002fb7aa42abd4cb05c9ccd3
nabanitkoirala <nabanitkoirala@gmail.com>
2021-04-13 16:41:32 +0545

    table data made dynamic

--- src/views/PalikaReport/components/palikaReportTable.tsx
+++ src/views/PalikaReport/components/palikaReportTable.tsx
@@ -3,10 +3,11 @@ import ReactHTMLTableToExcel from 'react-html-table-to-excel';
 import styles from './styles.scss';
 import Icon from '#rscg/Icon';
 
+
 const PalikaReportTable = (props) => {
-    console.log('This props>>>', props.tableData);
-    const { paginationData, tableData, tableHeader } = props;
-    console.log('What that>>>', paginationData);
+    const { paginationData, tableData, tableHeader, tableHeaderDataMatch } = props;
+
+
     return (
         <div>
             {/* <h1>Responsive Table Example</h1> */}
@@ -14,41 +15,30 @@ const PalikaReportTable = (props) => {
                 <table id="table-to-xls">
                     <tbody>
                         <tr>
-                            {tableHeader.map((item => (
-
-                                <th key={item}>{item}</th>
-
-                            )))}
+                            {tableHeader.map((item, i) => (
+                                <th key={item.i}>{item}</th>
+                            ))}
                         </tr>
-                        {/* <tr>
-                            <th>S/N</th>
-                            <th>Province</th>
-                            <th>District</th>
-                            <th>Municipality</th>
-                            <th>Fiscal Year</th>
-                            <th>Disaster Budget (Nrs)</th>
-                            <th>Other Budget (Nrs)</th>
-                            <th>Total Budget (Nrs)</th>
-
-                        </tr> */}
-
-
-                        {tableData.map((item, i) => (
-                            <tr key={item.item.id}>
-                                <td>{i + 1}</td>
-                                <td>{item.provinceName}</td>
-                                <td>{item.districtName}</td>
-                                <td>{item.municipalityName}</td>
-                                <td>{item.item.fiscalYear}</td>
-                                <td>{item.item.disasterBudgetNrs}</td>
-                                <td>{item.item.otherBudgetNrs}</td>
-                                <td>{item.item.totalBudgetNrs}</td>
+                        {tableData.map(data => (
+                            <tr key={data.item.id}>
+                                {tableHeaderDataMatch.map(title => (
+                                    <td key={title}>{title === 'province' || title === 'district' || title === 'municipality' || title === 'fiscalYear' ? data[title] : String(data.item[title])}</td>
+                                ))}
                             </tr>
                         ))}
-
                     </tbody>
                 </table>
-
+                {tableData && tableData.length !== 0
+                && (
+                    <ReactHTMLTableToExcel
+                        id="test-table-xls-button"
+                        className="download-table-xls-button"
+                        table="table-to-xls"
+                        filename="tablexls"
+                        sheet="tablexls"
+                        buttonText="XLS"
+                    />
+                )}
                 {tableData && tableData.length === 0
 && <p className={styles.dataUnavailable}>Data Unavailable</p>
 
