4fad675ccae6e7f70fecb2ab7361b7eaa6d7c701
Ankur Acharya <ankur@youthinnovationlab.org>
2021-04-18 20:27:53 +0545

    report styling begin

--- src/views/PalikaReport/ReportModal/Sections/Preview/PageOne/index.tsx
+++ src/views/PalikaReport/ReportModal/Sections/Preview/PageOne/index.tsx
@@ -81,7 +81,7 @@ const Preview = (props: Props) => {
                                 fill="#8884d8"
                                 label
                             />
-                            <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
+                            <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" label />
                             <Tooltip />
                             <Legend />
 
