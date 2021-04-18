d6a0f036078c0406823fca82807b4339f6593917
nabanitkoirala <nabanitkoirala@gmail.com>
2021-04-18 17:10:27 +0545

    final update

--- src/views/PalikaReport/ReportModal/Sections/Inventory/index.tsx
+++ src/views/PalikaReport/ReportModal/Sections/Inventory/index.tsx
@@ -1,53 +1,215 @@
-import React from 'react';
-import { ComposedChart,
-    Line,
-    Area,
-    Bar,
-    YAxis,
-    XAxis,
-    CartesianGrid,
-    Legend,
-    Scatter,
-    ResponsiveContainer,
-    BarChart } from 'recharts';
+/* eslint-disable @typescript-eslint/camelcase */
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import ReactPaginate from 'react-paginate';
+import { reverseRoute, _cs } from '@togglecorp/fujs';
+import { useTheme } from '@material-ui/core';
+import { Item } from 'semantic-ui-react';
 import styles from './styles.scss';
-import chartData from './chartData';
+import {
+    createConnectedRequestCoordinator,
+    createRequestClient,
+    NewProps,
+    ClientAttributes,
+    methods,
+} from '#request';
+import { provincesSelector,
+    districtsSelector,
+    municipalitiesSelector,
+    userSelector } from '#selectors';
 
 interface Props{
-    width: string;
-    height?: string;
 
 }
+const mapStateToProps = (state, props) => ({
+    provinces: provincesSelector(state),
+    districts: districtsSelector(state),
+    municipalities: municipalitiesSelector(state),
+    user: userSelector(state),
+});
+const requests: { [key: string]: ClientAttributes<ReduxProps, Params>} = {
+    PalikaReportInventoriesReport: {
+        url: ({ params }) => `${params.url}`,
+        query: ({ params, props }) => {
+            if (params && params.user) {
+                return {
+                    province: params.user.profile.province,
+                    district: params.user.profile.district,
+                    municipality: params.user.profile.municipality,
+                    // limit: params.page,
+                    resource_type: params.inventories,
+                    expand: params.fields,
+                    meta: params.meta,
 
-const Inventory = (props: Props) => {
-    const { width, height } = props;
+                };
+            }
+
+
+            return { limit: params.page,
+                offset: params.offset,
+                resource_type: params.inventories,
+                expand: params.fields,
+                meta: params.meta };
+        },
+        method: methods.GET,
+        onMount: true,
+
+        onSuccess: ({ response, params }) => {
+            let citizenReportList: CitizenReport[] = [];
+            const citizenReportsResponse = response as MultiResponse<CitizenReport>;
+            citizenReportList = citizenReportsResponse.results;
+
+            if (params && params.organisation) {
+                params.organisation(citizenReportList);
+            }
+            if (params && params.paginationParameters) {
+                params.paginationParameters(response);
+            }
+        },
+    },
+};
+
+
+const Organisation: React.FC<Props> = (props: Props) => {
+    const [fetchedData, setFetechedData] = useState([]);
+    const [tableHeader, setTableHeader] = useState([]);
+    const [paginationParameters, setPaginationParameters] = useState();
+    const [paginationQueryLimit, setPaginationQueryLimit] = useState(5);
+    const [offset, setOffset] = useState(0);
+    const [url, setUrl] = useState('/resource/');
+    const { requests: { PalikaReportInventoriesReport }, provinces,
+        districts,
+        municipalities,
+        user } = props;
+    const [defaultQueryParameter, setDefaultQueryParameter] = useState('governance');
+    const [fields, setfields] = useState('inventories');
+    const [meta, setMeta] = useState(true);
+    const handleFetchedData = (response) => {
+        setFetechedData(response);
+    };
+    const handlePaginationParameters = (response) => {
+        setPaginationParameters(response);
+    };
+    const handlePageClick = (e) => {
+        const selectedPage = e.selected;
+
+        setOffset(selectedPage * 2);
+    };
+    PalikaReportInventoriesReport.setDefaultParams({
+        organisation: handleFetchedData,
+        paginationParameters: handlePaginationParameters,
+        url,
+        page: paginationQueryLimit,
+        inventories: defaultQueryParameter,
+        fields,
+        user,
+        meta,
+
+    });
+    console.log('hang fetch data>>>', fetchedData);
+
+    useEffect(() => {
+        PalikaReportInventoriesReport.do({
+            offset,
+        });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [offset]);
+    // Finding Header for table data
+
+
     return (
-        <>
-            <p><strong>Disaster Inventory</strong></p>
-            <ResponsiveContainer width={width} height={height}>
-                <BarChart
-                    width={300}
-                    height={600}
-                    data={chartData.disasterInventory}
-                    layout="vertical"
-                    margin={{ left: 20, right: 20 }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis type="number" />
-                    <YAxis
-                        type="category"
-                        dataKey="name"
-                        tick={{ fill: '#94bdcf' }}
-                    />
-                    {/* <Tooltip /> */}
-                    {/* <Legend /> */}
-                    <Bar dataKey="Functional" stackId="a" fill="#8884d8" />
-                    <Bar dataKey="Non-Functional" stackId="a" fill="#82ca9d" />
-                </BarChart>
-            </ResponsiveContainer>
+        <div className={styles.tabsPageContainer}>
+            <p>
+                <strong>
+                DRR related organizations in Municipal Government
+                </strong>
+            </p>
+            <div className={styles.palikaTable}>
+                <table id="table-to-xls">
+                    <tbody>
+                        <tr>
 
-        </>
+                            <th>S.N</th>
+                            <th>Name of resource</th>
+                            <th>Quantity</th>
+                            <th>Owner Organization</th>
+                            <th>Type of Organization</th>
+                            <th>Added Date</th>
+
+                        </tr>
+
+                        {fetchedData.map(item => (
+                            item.inventories.map((data, i) => (
+                                <tr>
+                                    <td>
+                                        {i + 1}
+                                    </td>
+                                    <td>{data.item.title}</td>
+                                    <td>{data.quantity}</td>
+                                    <td>
+                                        {item.title}
+                                    </td>
+                                    <td>{item.type}</td>
+                                    <td>{data.createdOn}</td>
+                                </tr>
+                            ))
+
+                        ))}
+                        {/* {fetchedData.map(array => (
+
+                            array.map((data, i) => (
+
+                                <tr key={data.item.id}>
+                                    <td>{i + 1}</td>
+                                    <td>{data.item.title}</td>
+                                    <td>{data.quantity}</td>
+                                    <td />
+
+                                    <td>
+
+                                        {data.item.category}
+                                    </td>
+                                </tr>
+                            ))
+
+                        ))} */}
+
+
+                    </tbody>
+                </table>
+                {paginationParameters && paginationParameters.count !== 0
+                            && (
+                                <div className={styles.paginationRight}>
+                                    <ReactPaginate
+                                        previousLabel={'prev'}
+                                        nextLabel={'next'}
+                                        breakLabel={'...'}
+                                        breakClassName={'break-me'}
+                                        onPageChange={handlePageClick}
+                                        marginPagesDisplayed={2}
+                                        pageRangeDisplayed={5}
+                                        pageCount={Math.ceil(paginationParameters.count
+                                         / paginationQueryLimit)}
+                                        containerClassName={styles.pagination}
+                                        subContainerClassName={_cs(styles.pagination)}
+                                        activeClassName={styles.active}
+                                    />
+                                </div>
+                            )}
+                {fetchedData && fetchedData.length === 0
+                && <p className={styles.dataUnavailable}>Data Unavailable</p>
+
+                }
+            </div>
+
+        </div>
     );
 };
 
-export default Inventory;
+export default connect(mapStateToProps)(
+    createConnectedRequestCoordinator<PropsWithRedux>()(
+        createRequestClient(requests)(
+            Organisation,
+        ),
+    ),
+);
