72f0126755a6c97ae5003b570ae40977ad743f1c
Ankur Acharya <ankur@youthinnovationlab.org>
2021-04-20 16:51:19 +0545

    tabs refactored

---
+++ src/views/PalikaReport/MainModal/tabs.ts
@@ -0,0 +1,76 @@
+export default {
+    tabs: [
+        {
+            key: 0,
+            content: 'General',
+            url: '/municipality-contact/',
+        },
+        {
+            key: 1,
+            content: 'Budget',
+            url: '/annual-budget/',
+        },
+        {
+            key: 2,
+            content: 'Budget Activity',
+            url: '/annual-budget-activity/',
+        },
+        {
+            key: 3,
+            content: 'Programme and Policies',
+            url: '/annual-policy-program/',
+        },
+        {
+            key: 4,
+            content: 'Organisation',
+            url: '/resource/',
+        },
+        {
+            key: 5,
+            content: 'Inventories',
+            url: '',
+        },
+        {
+            key: 6,
+            content: 'Resources',
+            url: '',
+        },
+        {
+            key: 7,
+            content: 'Contacts',
+            url: '',
+        },
+        {
+            key: 8,
+            content: 'Relief',
+            url: '',
+        },
+        {
+            key: 9,
+            content: 'Incident',
+            url: '',
+        },
+        {
+            key: 10,
+            content: 'Simulation',
+            url: '/simulation/',
+        },
+        {
+            key: 11,
+            content: 'Loss & Damage',
+            url: '',
+        },
+        {
+            key: 12,
+            content: 'Preparedness',
+            url: '',
+        },
+
+        {
+            key: 13,
+            content: 'Preview',
+            url: '',
+        },
+    ],
+
+};
