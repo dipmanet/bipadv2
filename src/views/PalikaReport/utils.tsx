9309bf6266405ad7648c046ffffaf1bfa962d1c1
Ankur Acharya <ankur@youthinnovationlab.org>
2021-05-10 09:18:39 +0545

    added types declarations in the translations function

--- src/views/PalikaReport/utils.tsx
+++ src/views/PalikaReport/utils.tsx
@@ -2,13 +2,26 @@ import { connect } from 'react-redux';
 import {
     palikaLanguageSelector,
 } from '#selectors';
+import { AppState } from '#store/types';
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state: AppState) => ({
     palikaLanguage: palikaLanguageSelector(state),
 });
 
+interface PalikaLanguage{
+    language: 'en' | 'np';
+}
 
-const Gt = (props) => {
+interface Section {
+    np: string;
+    en: string;
+}
+interface Props {
+    section: Section;
+    palikaLanguage: PalikaLanguage;
+}
+
+const Gt = (props: Props) => {
     const {
         palikaLanguage,
         section,
