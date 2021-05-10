f7ead49c04047c9a3b3e61971eb7ee64cfee7ddc
Ankur Acharya <ankur@youthinnovationlab.org>
2021-05-10 10:02:22 +0545

    added type declaration in translation file

--- src/views/PalikaReport/Translations.ts
+++ src/views/PalikaReport/Translations.ts
@@ -1,4 +1,11 @@
-const languageTranslations = {
+interface Section {
+    np: string;
+    en: string;
+}
+interface LanguageEntry {
+    [key: string]: Section;
+}
+const languageTranslations: LanguageEntry = {
     dashBoardMainTitle: {
         np: 'प्रकोप जोखिम न्यूनीकरण र व्यवस्थापन रिपोर्ट',
         en: 'Disaster Risk Reduction and Management Report',
