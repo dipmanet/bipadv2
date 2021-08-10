e0cf2c8543bf1ae282cdca09eef84f7ac9936350
nabanitkoirala <nabanitkoirala@gmail.com>
2021-08-10 12:50:20 +0545

    update

--- src/views/RiskInfo/LeftPane/Details/Hazard/index.tsx
+++ src/views/RiskInfo/LeftPane/Details/Hazard/index.tsx
@@ -5,20 +5,12 @@ import {
     _cs,
 } from '@togglecorp/fujs';
 
-import { connect } from 'react-redux';
 import LayerSelection from '#components/LayerSelection';
 import { LayerWithGroup, LayerGroup, HazardType } from '#store/atom/page/types';
 
 import { getLayerHierarchy } from '#utils/domain';
 
 import styles from './styles.scss';
-import {
-    createConnectedRequestCoordinator,
-    createRequestClient,
-    ClientAttributes,
-    methods,
-} from '#request';
-import { municipalitiesSelector } from '#selectors';
 
 interface Props {
     className?: string;
@@ -29,54 +21,8 @@ interface Props {
 
 interface State {
 }
-const requests: { [key: string]: ClientAttributes<ReduxProps, Params> } = {
-    FeatureGetMunicipalityImages: {
-        url: '/municipality-images/',
-        method: methods.GET,
-        onMount: true,
-
-        onSuccess: ({ response, params }) => {
-            // params.responseData(response);
-            if (params) {
-                params.municipalityData(response.results);
-            }
-        },
-    },
-
-
-};
-const mapStateToProps = (state: AppState): PropsFromState => ({
-
-    municipalities: municipalitiesSelector(state),
-
-    // hazardList: hazardTypeListSelector(state),
-});
-
 
 class Hazard extends React.PureComponent<Props, State> {
-    public constructor(props) {
-        super(props);
-        this.state = {
-            municipalityImagedata: [],
-        };
-    }
-
-    private municipalityImageData=(data) => {
-        const { municipalities } = this.props;
-        const { handleLandslidePolygonImageMap } = this.context;
-        const munDataWithDistrict = data.map((item) => {
-            const dist = municipalities
-                .filter(mun => mun.id === item.municipality)
-                .map(rslt => rslt.district);
-            return ({
-                ...item,
-                district: dist[0],
-            });
-        });
-        handleLandslidePolygonImageMap(munDataWithDistrict);
-        this.setState({ municipalityImagedata: munDataWithDistrict });
-    }
-
     private getHierarchy = memoize(getLayerHierarchy);
 
     public render() {
@@ -84,20 +30,12 @@ class Hazard extends React.PureComponent<Props, State> {
             className,
             layerList,
             layerGroupList,
-            requests: { FeatureGetMunicipalityImages },
-            municipalities,
         } = this.props;
-        const { municipalityImagedata } = this.state;
-        const { landslidePolygonImagemap } = this.context;
+
         const layers = this.getHierarchy(
             layerList,
             layerGroupList,
         );
-        FeatureGetMunicipalityImages.setDefaultParams({
-            municipalityData: this.municipalityImageData,
-        });
-        console.log('test', landslidePolygonImagemap);
-
 
         return (
             <LayerSelection
@@ -108,11 +46,4 @@ class Hazard extends React.PureComponent<Props, State> {
     }
 }
 
-
-export default connect(mapStateToProps)(
-    createConnectedRequestCoordinator<PropsWithRedux>()(
-        createRequestClient(requests)(
-            Hazard,
-        ),
-    ),
-);
+export default Hazard;
