b46ade9f8d14b29d6abcfa990d4b9fb0023d785c
Ankur Acharya <ankur@youthinnovationlab.org>
2021-08-19 16:32:59 +0545

    added permissions function

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -232,3 +232,38 @@ export const httpGet = (url: string) => {
     xmlHttp.send(null);
     return xmlHttp.response;
 };
+
+// user object from redux, example codeName="edit_resource", app="resource"
+export const checkPermission = (user, codeName, app) => {
+    let permission = false;
+    if (!user) {
+        permission = false;
+    } else if (user.isSuperuser) {
+        permission = true;
+    }
+    if (user && user.groups) {
+        user.groups.forEach((group) => {
+            if (group.permissions) {
+                group.permissions.forEach((p) => {
+                    if (p.codename === codeName && p.app === app) {
+                        permission = true;
+                    }
+                });
+            } else {
+                permission = false;
+            }
+        });
+    }
+    if (user && user.userPermissions) {
+        user.userPermissions.forEach((a) => {
+            if (a.codename === codeName && a.app === app) {
+                permission = true;
+            }
+        });
+    } else {
+        permission = false;
+    }
+    return permission;
+    // temporary set true to all user for testing
+    // return true;
+};
