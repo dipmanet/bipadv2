e68772c6edf4e13a4d1449715d1dba79a3af36c5
Ankur Acharya <ankur@youthinnovationlab.org>
2022-03-02 15:01:53 +0545

    language setup

---
+++ src/components/LanguageToggle/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import Redux from 'redux';
+
+import { connect } from 'react-redux';
+import { _cs } from '@togglecorp/fujs';
+import * as PageTypes from '#store/atom/page/types';
+
+import {
+    languageSelector,
+} from '#selectors';
+
+
+import {
+    setLanguageAction,
+} from '#actionCreators';
+
+import styles from './styles.scss';
+import { AppState } from '#types';
+
+interface ComponentProps {
+}
+
+interface PropsFromAppState {
+    language: PageTypes.Language;
+}
+
+type Props = ComponentProps & PropsFromAppState & PropsFromDispatch ;
+
+interface PropsFromDispatch {
+    setLanguage: typeof setLanguageAction;
+}
+
+const mapStateToProps = (state: AppState): PropsFromAppState => ({
+    language: languageSelector(state),
+});
+const mapDispatchToProps = (dispatch: Redux.Dispatch): PropsFromDispatch => ({
+    setLanguage: params => dispatch(setLanguageAction(params)),
+});
+
+const LanguageToggle = (props: Props) => {
+    const { language: { language }, setLanguage } = props;
+
+    const handleLangButton = (val: string) => {
+        setLanguage({ language: val });
+    };
+
+    return (
+        <div className={styles.languageButton}>
+            <button
+                onClick={() => handleLangButton('en')}
+                className={language === 'en' ? _cs(styles.engButton, styles.selectedLanguage) : styles.engButton}
+                type="button"
+            >
+                EN
+            </button>
+            <button
+                onClick={() => handleLangButton('np')}
+                className={language === 'np' ? _cs(styles.nepButton, styles.selectedLanguage) : styles.nepButton}
+                type="button"
+            >
+                ने
+            </button>
+        </div>
+    );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LanguageToggle);
