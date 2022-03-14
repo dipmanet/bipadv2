8bbe2e43231dfef2ca129577e52cebe198f3e944
nabanitkoirala <nabanitkoirala@gmail.com>
2022-03-14 11:07:45 +0545

    update

--- src/Multiplexer/DownloadButtonOption/index.tsx
+++ src/Multiplexer/DownloadButtonOption/index.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import Redux from 'redux';
 import { connect } from 'react-redux';
 import { _cs } from '@togglecorp/fujs';
 
+import Faram from '@togglecorp/faram';
 import ReduxContext from '#components/ReduxContext';
 
 import osmLibertyStyle from '#mapStyles/style';
@@ -15,15 +18,15 @@ import { setMapStyleAction } from '#actionCreators';
 import { mapStyleSelector } from '#selectors';
 
 import LayerButton from './LayerButton';
-
 // Icons
 import OutLineIcon from '#resources/images/outline.png';
 import MapboxLightIcon from '#resources/images/mapbox-light.png';
 import MapboxRoadsIcon from '#resources/images/mapbox-roads.png';
 import MapboxSatelliteIcon from '#resources/images/mapbox-satellite.png';
 import OSMIcon from '#resources/images/osm.png';
-
+import Button from '#rsca/Button';
 import styles from './styles.scss';
+import SelectInput from '#rsci/SelectInput';
 
 const mapStyles = [
     {
@@ -68,8 +71,20 @@ const mapDispatchToProps = (dispatch: Redux.Dispatch): PropsFromDispatch => ({
 });
 
 const layerKeySelector = (d: MapStyle) => d.name;
-
+const keySelector = (d: KeyLabel) => d.key;
+const labelSelector = (d: KeyLabel) => d.label;
 class LayerSwitch extends React.PureComponent<Props, State> {
+    public constructor(props: Props) {
+        super(props);
+        this.state = {
+            faramValues: {
+                customType: '',
+            },
+            faramErrors: {},
+        };
+    }
+
+
     private isActiveMapStyle = (styleFromLayer) => {
         const { currentMapStyle } = this.props;
 
@@ -97,9 +112,21 @@ class LayerSwitch extends React.PureComponent<Props, State> {
         }
     }
 
+    private handleFaramChange = (faramValues: FaramValues, faramErrors: FaramErrors) => {
+        console.log('faram value', faramValues);
+        this.setState({
+            faramValues: {
+                customType: faramValues,
+            },
+            faramErrors,
+
+        });
+    }
+
     public render() {
         const { className } = this.props;
-
+        const { faramValues, faramErrors } = this.state;
+        const booleanCondition = [{ key: true, label: 'Yes' }, { key: false, label: 'No' }];
         return (
             <DropdownMenu
                 className={_cs(styles.layerSwitch, className)}
@@ -107,12 +134,47 @@ class LayerSwitch extends React.PureComponent<Props, State> {
                 hideDropdownIcon
                 tooltip="Download current map"
             >
-                <ListView
+                <div className={styles.mainContainer}>
+                    <div className={styles.heading}>
+                        <h3>Download</h3>
+                    </div>
+                    <div className={styles.buttonOptions}>
+                        <Button
+                            className={styles.downloadButton}
+                        >
+                            Download with default settings
+                        </Button>
+                        <Button
+                            className={styles.downloadButton}
+                        >
+                            Download with custom settings
+                        </Button>
+                        <div>
+                            <form className={styles.dropdown}>
+                                <label htmlFor="Custom">
+                                    Custom:
+                                </label>
+                                <select name="custom" id="custom">
+                                    <option value="volvo">Page Type</option>
+                                    <option value="saab">Resolution Type</option>
+
+                                </select>
+
+
+                            </form>
+
+                        </div>
+                    </div>
+
+
+                </div>
+
+                {/* <ListView
                     data={mapStyles}
                     keySelector={layerKeySelector}
                     renderer={LayerButton}
                     rendererParams={this.getLayerButtonRendererParams}
-                />
+                /> */}
             </DropdownMenu>
         );
     }
