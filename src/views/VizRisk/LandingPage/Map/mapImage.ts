ec9fcbe62fd6dfdac98d329ee740f2bf48183c8d
Achyut <achyutpaudel24@gmail.com>
2022-08-23 12:08:28 +0545

    finalised vizrisk landing page

---
+++ src/views/VizRisk/LandingPage/Map/mapImage.ts
@@ -0,0 +1,20 @@
+import Flood from '#resources/icons/flood.png';
+import Landslide from '#resources/icons/landslide.png';
+import Multihazard from '#resources/icons/multihazard.png';
+
+const landingPageMapImages = [
+    {
+        name: 'flood',
+        url: Flood,
+    },
+    {
+        name: 'landslide',
+        url: Landslide,
+    },
+    {
+        name: 'multihazard',
+        url: Multihazard,
+    },
+];
+
+export default landingPageMapImages;
---
+++ src/views/VizRisk/expressions.ts
@@ -0,0 +1,21 @@
+const tourContents = [
+    {
+        tourTitle: 'VisRisk',
+        tourContent: `Simple Yet Creative way of visualizing disaster-related
+        information through text, cgarts, ingographics, and maps using story
+        telling format.`,
+    },
+    {
+        tourTitle: 'VisRisk',
+        tourContent: `An Interactive tool to visualize data and information on hazard
+        ,exposure and vulnerability for informed decision making in Disaster Risk Reduction 
+        and Management
+        `,
+    },
+    {
+        tourTitle: 'VisRisk',
+        tourContent: 'VisRisk has benn developed for 11 municipalities and two provinces of Nepal',
+    },
+
+];
+export default tourContents;
