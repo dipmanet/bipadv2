b14a90a8c21141b27cd09ab2c2f4c2f300f02162
Kapil Kandel <kandelkapil01@gmail.com>
2022-09-24 09:57:52 +0545

    test paint generate added

--- src/views/LossAndDamage/Map/index.js
+++ src/views/LossAndDamage/Map/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { _cs } from '@togglecorp/fujs';
-
+import memoize from 'memoize-one';
 import Numeral from '#rscv/Numeral';
 
 import SegmentInput from '#rsci/SegmentInput';
@@ -9,7 +9,7 @@ import ChoroplethMap from '#components/ChoroplethMap';
 import { lossMetrics } from '#utils/domain';
 
 import styles from './styles.scss';
-import Legend from '../Legend';
+import Legend, { legentItemsTest } from '../Legend';
 import { generateColor,
     generatePaint,
     generateMapState,
@@ -44,10 +44,38 @@ export default class LossAndDamageMap extends React.PureComponent {
             radioSelect,
             currentSelection,
         } = this.props;
-
-        const color = generateColor(maxValue, 0, colorGrade);
-        const colorPaint = generatePaint(color);
         const mapState = generateMapState(geoareas, mapping, metric);
+        const mapStateValue = mapState.map(i => i.value);
+
+        const generateColorTest = memoize((valueFromData, valueToBeCompared) => {
+            const color = [];
+            // eslint-disable-next-line no-restricted-syntax
+            for (const element of valueToBeCompared) {
+                // eslint-disable-next-line no-restricted-syntax
+                for (const valueOfData of valueFromData) {
+                    // eslint-disable-next-line no-eval
+                    if (eval(`${valueOfData}${element.value[0]}`)
+                    // eslint-disable-next-line no-eval
+                    && eval(`${valueOfData}${element.value[1]}`)) {
+                        const colorFromElement = element.color;
+                        color.push(valueOfData, colorFromElement);
+                    }
+                }
+            }
+            return color;
+        });
+        const valueToBeCompared = legentItemsTest.map(i => (
+            {
+                value: i.value,
+                color: i.color,
+            }
+        ));
+        const color = generateColorTest(mapStateValue, valueToBeCompared);
+        // const color = generateColor(maxValue, 0, colorGrade);
+        const colorPaint = color.length > 0 && generatePaint(color);
+        console.log(colorPaint, 'color');
+
+
         const colorUnitWidth = `${100 / colorGrade.length}%`;
         // const colorString = `linear-gradient(to right, ${pickList(color, 1, 2).join(', ')})`;
 
